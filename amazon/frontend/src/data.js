ec32ed6cb47585563efe0e6fd035fab5eb600ada
Madhav <2021ec0473@svce.ac.in>
2024-03-21 22:53:14 +0530

    4

---
+++ amazon/frontend/src/data.js
@@ -0,0 +1,50 @@
+const data={
+    products:[
+        {
+            name:"Nike Slim Shirt",
+            slug:"Nike-Slim-Shirt",
+            category:"Shirts",
+            image:"/images/p1.jpg",
+            price:120,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product",
+        },
+        {
+            name:"adidas Slim Shirt",
+            slug:"adidas-Slim-Shirt",
+            category:"Shirts",
+            image:"/images/p2.jpg",
+            price:120,
+            brand:"adidas",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product",
+        },
+        {
+            name:"Nike pant",
+            slug:"Nike-pant",
+            category:"pant",
+            image:"/images/p3.jpg",
+            price:120,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product",
+        },
+        {
+            name:"adidas pant",
+            slug:"adidas-pant",
+            category:"pant",
+            image:"/images/p4.jpg",
+            price:120,
+            brand:"adidas",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product",
+        },  
+    ],
+};
+
+export default data;
